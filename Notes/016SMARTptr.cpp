5b7184f89356b99f957136622110ce3c8fd8ad21
yzhbradoodrrpurp <zhihangyi0224@gmail.com>
2025-04-24 13:06:47 +0800

    Add a note for smart ptr.

--- Notes/016SMARTptr.cpp
+++ Notes/016SMARTptr.cpp
@@ -1,3 +1,26 @@
 //
 // Created by 易治行 on 2025/4/23.
 //
+
+/*
+ * 智能指针分为三类：
+ * - `unique_ptr`: 独占所有权，同一时刻只能有一个指针指向同一个对象，不能复制和赋值，通过 `move` 方法转移所有权
+ * - `shared_ptr`: ...
+ * - `weak_ptr`: ...
+ */
+
+#include <memory>
+#include <iostream>
+
+
+int main(void) {
+
+    std::unique_ptr<int> p_1(new int(10));
+    std::unique_ptr<int> p_2 = std::move(p_1);
+
+    std::cout << *p_2 << std::endl;
+
+    //todo: finish smart ptr... maybe next week...
+
+    return 0;
+}
