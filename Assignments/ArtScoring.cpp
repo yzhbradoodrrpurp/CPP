5fa53fffbde13581c59d3f9c7282e34aa5b42bf5
yzhbradoodrrpurp <zhihangyi0224@gmail.com>
2025-06-11 17:40:19 +0800

    update an assignment

--- Assignments/ArtScoring.cpp
+++ Assignments/ArtScoring.cpp
@@ -6,102 +6,73 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-
+#include <random>
 
 double accumulate_add(double accumulated_value, double next_value) {
     return accumulated_value + next_value;
+}
+
+bool sort_strategy(double a, double b) {
+    return a < b;
 }
 
 
 class ArtScoring {
 
 private:
-    std::vector<double> scores;
+    std::vector<std::vector<double>> scores;
 
 public:
-    ArtScoring() {}
+    ArtScoring(int vase) {
+        for (int i = 0; i < vase; i++)
+        scores.push_back(std::vector<double>());
+    }
     ~ArtScoring(){}
 
-    void add_score(double score) {
-        scores.push_back(score);
+    void add(int idx, double score) {
+        scores[idx].push_back(score);
     }
 
-    std::vector<double> valid_scores() {
-        auto valid(scores);
+    std::vector<double> valid_scores(int idx) {
+        std::sort(scores[idx].begin(), scores[idx].end(), sort_strategy);
 
-        std::sort(valid.begin(), valid.end());
-        valid.pop_back();
-        valid.erase(valid.begin() + 0);
+        std::vector<double> valid;
+
+        for (int i = 1; i < scores[idx].size() - 1; i++)
+            valid.push_back(scores[idx][i]);
 
         return valid;
     }
 
-    double valid_avg() {
-        auto valid = valid_scores();
+    double valid_average_scores(int idx) {
+        auto valid = valid_scores(idx);
+        auto sum = std::accumulate(valid.begin(), valid.end(), 0,accumulate_add);
+        int len = valid.size();
 
-        auto valid_sum = std::accumulate(valid.begin(), valid.end(), 0.0, accumulate_add);
-        int length = valid.size();
-
-        return valid_sum / length;
+        return sum / len;
     }
 
 };
 
 
 int main() {
-    auto vase1 = new ArtScoring();
-    auto vase2 = new ArtScoring();
-    auto vase3 = new ArtScoring();
-    auto vase4 = new ArtScoring();
-    auto vase5 = new ArtScoring();
+    auto as = ArtScoring(5);
 
-    vase1->add_score(100);
-    vase1->add_score(97);
-    vase1->add_score(87);
-    vase1->add_score(88);
-    vase1->add_score(90);
+    for (int vase = 0; vase < 5; vase++) {
+        for (int judge = 0; judge < 5; judge++) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> distrib(0, 100);
 
-    vase2->add_score(99);
-    vase2->add_score(96);
-    vase2->add_score(83);
-    vase2->add_score(89);
-    vase2->add_score(90);
+            double score = distrib(gen);
+            as.add(vase, score);
+        }
+    }
 
-    vase3->add_score(98);
-    vase3->add_score(95);
-    vase3->add_score(84);
-    vase3->add_score(88);
-    vase3->add_score(90);
-
-    vase4->add_score(97);
-    vase4->add_score(94);
-    vase4->add_score(85);
-    vase4->add_score(89);
-    vase4->add_score(90);
-
-    vase5->add_score(96);
-    vase5->add_score(93);
-    vase5->add_score(86);
-    vase5->add_score(89);
-    vase5->add_score(90);
-
-    auto valid_avg1 = vase1->valid_avg();
-    auto valid_avg2 = vase2->valid_avg();
-    auto valid_avg3 = vase3->valid_avg();
-    auto valid_avg4 = vase4->valid_avg();
-    auto valid_avg5 = vase5->valid_avg();
-
-    std::cout << "valid avg: " << valid_avg1 << std::endl;
-    std::cout << "valid avg: " << valid_avg2 << std::endl;
-    std::cout << "valid avg: " << valid_avg3 << std::endl;
-    std::cout << "valid avg: " << valid_avg4 << std::endl;
-    std::cout << "valid avg: " << valid_avg5 << std::endl;
-
-    delete vase1;
-    delete vase2;
-    delete vase3;
-    delete vase4;
-    delete vase5;
+    for (int vase = 0; vase < 5; vase++) {
+        auto valid_avg = as.valid_average_scores(vase);
+        std::cout << "vase " << vase << ", valid average scores " << valid_avg << std::endl;
+    }
 
     return 0;
 }
